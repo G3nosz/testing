3d703aa6480e97d0ca2e1e5e832714b7432d6346
Matas Veckas <matasveckas@gmail.com>
2021-11-16 11:53:17 +0200

    idk

--- SVS/TestProject1/IndexPageTests.cs
+++ SVS/TestProject1/IndexPageTests.cs
@@ -28,11 +28,11 @@ namespace TestProject1
         public void OnPost_IfInvalidModel_ReturnBadRequest()
         {
             //arrange
-            var pageModel = new LoginModel(_signInManager, _logger, _userManager);
+            var loginModel = new LoginModel(_signInManager, _logger, _userManager);
 
             //act
-            pageModel.ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-            var result = pageModel.OnPostAsync();
+            loginModel.ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            var result = loginModel.OnPostAsync();
 
             //assert
             Assert.IsType<BadRequestResult>(result); //problema nes pas mus ne bad request grazina bet kazkokia tai nurodita message
@@ -41,36 +41,21 @@ namespace TestProject1
         [Fact]
         public void OnPost_IfValidModel_ReturnPage()
         {
-            var options = new DbContextOptionsBuilder<LibDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestNewListDb").Options;
-            //reikia issiaiskint kaip cia pasiekt, koks tiksliai yra vardas ir klausimas ar man isviso reikia pasiekt ja?
-            //bet manau, kad reikia vistiek, bet nezinau kam realiai, jei uztenka tik response gaut ar gavau ismetama error ar
-            //ar man yra atidaromas kazkoks kistas page, nebent tikrint _logger.LogInformation -> kadangi ten yra rasoma zinute jei pavyko
-
-
-            //nesekmes atveju i ModelState.AddModelError prideda zinute "Invalid login attempt"
-
-
             //arrange
-            var pageModel = new LoginModel(_signInManager, _logger, _userManager);
-          
+            var loginModel = new LoginModel(_signInManager, _logger, _userManager);
 
-            InputModel input = new InputModel() //wtf kodel nepasiekia nors toj paciojdalyje 
-            {
-                Username = "Admin",
-                Password = "Testing2@",
-                RememberMe = false
-            }; //as jo pasiekt taip negaliu nes ta klase yra tarp LoginModel klases tai ji yra jos viduj
-            //kaip yra daromas post is to inputo pasiima tuomet username, pass ir t.t.
-            // ir tuomet paduoda per _signInManager ir tikrina
+            loginModel.Input.Username = "Admin";
+            loginModel.Input.Password = "Testing2@";
+            loginModel.Input.RememberMe = false;
 
 
             //act                
-            var result = pageModel.OnPostAsync();
+            var result = loginModel.OnPostAsync();
 
             //assert
             Assert.IsType<PageResult>(result);
         }
+
 
     }
 }
